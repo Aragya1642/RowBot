d6b7efd40b8e7adc0894ccb221dc535a0ad4c560
Aragya Goyal <56979762+Aragya1642@users.noreply.github.com>
2026-09-08 00:26:22 -0400

    mountain car setup

--- 2_RL_Tutorials/mountain_car.py
+++ 2_RL_Tutorials/mountain_car.py
@@ -1,4 +1,6 @@
 import gymnasium as gym
+from collections import defaultdict
+from tqdm import tqdm
 
 # Create agent class
 class MountainCarAgent:
@@ -13,9 +15,76 @@ class MountainCarAgent:
     ):
         self.env = env
 
+        # Create Q table
+        self.q_values = defaultdict()
 
+        self.lr = learning_rate
+        self.discount_factor = discount_factor
 
+        # Exploration Parameters
+        self.epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        # Track learning process
+        self.training_error = []
+
+    def get_action(self) -> int:
+        pass
+
+    def update(self):
+        pass
+
+    def decay_epsilon(self):
+        pass
+
+#################################################################
+# Train the agent
+#################################################################
+# Training hyperparameters
+learning_rate = 0.001
+n_episodes = 10
+start_epsilon = 1.0
+epsilon_decay = start_epsilon / (n_episodes / 2)
+final_epsilon = 0.1
 
 # Create environment
 env = gym.make("MountainCar-v0", render_mode="rgb_array", goal_velocity=0.1)
 print(env.action_space)
+
+# Create Agent
+agent = MountainCarAgent(
+    env=env,
+    learning_rate=learning_rate,
+    initial_epsilon=start_epsilon,
+    epsilon_decay=epsilon_decay,
+    final_epsilon=final_epsilon,
+)
+
+for episode in tqdm(range(n_episodes)):
+    # Reset environment
+    obs, info = env.reset()
+    done = False
+
+    # Play an episode
+    while not done:
+
+
+        # Move to next state
+        done = terminated or truncated
+        obs = next_obs
+
+    # Reduce exploration rate
+    agent.decay_epsilon()
+
+#################################################################
+# Visualize training metrics
+#################################################################
+
+# TODO
+
+#################################################################
+# Test the agent
+#################################################################
+
+# TODO
